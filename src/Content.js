5a30aaf7857a3feb50c98ef7d1beca2422f86dbd
Maxwell Kapral <git@maxkapral.com>
2023-11-12 02:13:15 -0800

    traversal...sortof

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,18 @@
-import {Button, Card, CardContent, CardHeader, Grid, IconButton, Snackbar, TextField, Tooltip} from '@mui/material';
+import {
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    Container,
+    Grid,
+    IconButton,
+    Snackbar,
+    TextField,
+    Tooltip
+} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import {useEffect, useState} from 'react';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import Box from '@mui/material/Box';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import EditIcon from '@mui/icons-material/Edit';
@@ -24,6 +36,119 @@ async function getMember (id) {
     return await API.get('HttpApi', '/get', init);
 }
 
+function displayBirthday(data) {
+    return (
+        <Grid item xs={12} sm={4} md={4}>
+            <Typography variant="body2">
+                <b>Birthday:</b>
+            </Typography>
+            <Typography variant="body2">
+                {new Date(data['member']['birthday']).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' })}
+            </Typography>
+        </Grid>
+    );
+}
+
+function displayDeathday (data) {
+    return 'deathday' in data['member'] ? (
+        <Grid item xs={12} sm={4} md={4}>
+            <Typography variant="body2">
+                <b>Deathday:</b>
+            </Typography>
+            <Typography variant="body2">
+                {new Date(data['member']['deathday']).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' })}
+            </Typography>
+        </Grid>
+    ) : (
+        <>
+        </>
+    );
+}
+
+function displayEmail (data) {
+    return 'email' in data['member'] ? (
+        <Grid item xs={12} sm={4} md={4}>
+            <Typography variant="body2">
+                <b>Email:</b>
+            </Typography>
+            <Typography variant="body2">
+                {data['member']['email']}
+            </Typography>
+        </Grid>
+    ) : (
+        <>
+        </>
+    );
+}
+
+function displayPhones (data) {
+    if ('phones' in data['member'] && ('MOBILE' in data['member']['phones'] || 'LANDLINE' in data['member']['phones'])) {
+        if ('LANDLINE' in data['member']['phones'] && 'MOBILE' in data['member']['phones']) {
+            return (
+                <>
+                    <Grid item xs={12} sm={4} md={4}>
+                        <Typography variant="body2">
+                            <b>Mobile Phone:</b>
+                        </Typography>
+                        <Typography variant="body2">
+                            {data['member']['phones']['MOBILE']}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={4} md={4}>
+                        <Typography variant="body2">
+                            <b>Landline Phone:</b>
+                        </Typography>
+                        <Typography variant="body2">
+                            {data['member']['phones']['LANDLINE']}
+                        </Typography>
+                    </Grid>
+                </>
+            );
+        }else if ('MOBILE' in data['member']['phones']) {
+            return (
+                <Grid item xs={12} sm={4} md={4}>
+                    <Typography variant="body2">
+                        <b>Mobile Phone:</b>
+                    </Typography>
+                    <Typography variant="body2">
+                        {data['member']['phones']['MOBILE']}
+                    </Typography>
+                </Grid>
+            );
+        } else {
+            return (
+                <Grid item xs={12} sm={4} md={4}>
+                    <Typography variant="body2">
+                        <b>Landline Phone:</b>
+                    </Typography>
+                    <Typography variant="body2">
+                        {data['member']['phones']['LANDLINE']}
+                    </Typography>
+                </Grid>
+            );
+        }
+    } else {
+        return (<></>);
+    }
+}
+
+function displayAddress (data) {
+    if ('address' in data['member']) {
+        return (
+            <Grid item xs={12} sm={4} md={4}>
+                <Typography variant="body2">
+                    <b>Address:</b>
+                </Typography>
+                <Typography variant="body2" style={{whiteSpace: 'pre-line'}}>
+                    {data['member']['address'].join('\n')}
+                </Typography>
+            </Grid>
+        );
+    } else {
+        return (<></>);
+    }
+}
+
 export default function Content() {
     const [isLoading, setIsLoading] = useState(true);
     const [id, setId] = useState(null);
@@ -31,7 +156,7 @@ export default function Content() {
     const [caller, setCaller] = useState(null);
 
     useEffect(() => {
-        async function getData() {
+        async function getData(id) {
             const data = await getMember(id);
             if (id === null) {
                 setCaller(data);
@@ -39,7 +164,7 @@ export default function Content() {
             setData(data);
         }
         setIsLoading(true);
-        getData().then(() => setIsLoading(false));
+        getData(id).then(() => setIsLoading(false));
     }, [id]);
 
     if (isLoading) {
@@ -47,88 +172,29 @@ export default function Content() {
     }
 
     return (
-        <Card sx={{ width: '75%', margin: 'auto', display: 'block' }}>
-            <CardContent>
-                <CardHeader
-                    title={['firstName', 'middleName', 'lastName', 'suffix'].map(key => data.member[key]).filter(Boolean).join(' ')}
-                />
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={12} sm={4} md={4}>
-                        <Typography variant="body2">
-                            <b>Birthday:</b>
-                        </Typography>
-                        <Typography variant="body2">
-                            {new Date(data['member']['birthday']).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' })}
-                        </Typography>
+        <>
+            <IconButton
+                size="large"
+                aria-label="Download"
+                color="inherit"
+                onClick={() => setId(data['family']['ancestor'])}
+            >
+                <ArrowUpwardIcon />
+            </IconButton>
+            <Card sx={{ width: '75%', margin: 'auto', display: 'block' }}>
+                <CardContent>
+                    <CardHeader
+                        title={['firstName', 'middleName', 'lastName', 'suffix'].map(key => data['member'][key]).filter(Boolean).join(' ')}
+                    />
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        {displayBirthday(data)}
+                        {displayDeathday(data)}
+                        {displayEmail(data)}
+                        {displayPhones(data)}
+                        {displayAddress(data)}
                     </Grid>
-                    { 'deathday' in data['member'] ? (
-                        <Grid item xs={12} sm={4} md={4}>
-                            <Typography variant="body2">
-                                <b>Deathday:</b>
-                            </Typography>
-                            <Typography variant="body2">
-                                {new Date(data['member']['deathday']).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' })}
-                            </Typography>
-                        </Grid>
-                    ) : (
-                        <>
-                        </>
-                    )}
-                    { 'email' in data['member'] ? (
-                        <Grid item xs={12} sm={4} md={4}>
-                            <Typography variant="body2">
-                                <b>Email:</b>
-                            </Typography>
-                            <Typography variant="body2">
-                                {data['member']['email']}
-                            </Typography>
-                        </Grid>
-                    ) : (
-                        <>
-                        </>
-                    )}
-                    { 'phones' in data['member'] ?
-                        'MOBILE' in data['member']['phones'] ? (
-                            <Grid item xs={12} sm={4} md={4}>
-                                <Typography variant="body2">
-                                    <b>Mobile Phone:</b>
-                                </Typography>
-                                <Typography variant="body2">
-                                    {data['member']['phones']['MOBILE']}
-                                </Typography>
-                            </Grid>
-                        ) : 'LANDLINE' in data['member']['phones'] ? (
-                            <Grid item xs={12} sm={4} md={4}>
-                                <Typography variant="body2">
-                                    <b>Mobile Phone:</b>
-                                </Typography>
-                                <Typography variant="body2">
-                                    {data['member']['phones']['LANDLINE']}
-                                </Typography>
-                            </Grid>
-                        ) : (
-                            <>
-                            </>
-                        )
-                    : (
-                        <>
-                        </>
-                    )}
-                    { 'address' in data['member'] ? (
-                        <Grid item xs={12} sm={4} md={4}>
-                            <Typography variant="body2">
-                                <b>Address:</b>
-                            </Typography>
-                            <Typography variant="body2" style={{ whiteSpace: 'pre-line' }}>
-                                {data['member']['address'].join('\n')}
-                            </Typography>
-                        </Grid>
-                    ) : (
-                        <>
-                        </>
-                    )}
-                </Grid>
-            </CardContent>
-        </Card>
+                </CardContent>
+            </Card>
+        </>
     );
 }
